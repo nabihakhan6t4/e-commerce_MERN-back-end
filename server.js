cc84bfc1ac63d72b4318dad2a01ba8d10f7a23ec
nabihakhan6t4 <khannabiha923@gmail.com>
2025-04-13 16:31:41 +0300

    changes

--- server.js
+++ server.js
@@ -23,25 +23,39 @@ mongoose
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 const corsOptions = {
   origin: [
-    "https://nabihakhan6t4.github.io", 
-    "https://server-lhxpbq9qp-nabiha-khans-projects-954d6523.vercel.app" 
+    "http://localhost:5173",
+    "https://nabihakhan6t4.github.io",
+    "https://e-commerce-mern-back-end.vercel.app",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
+ 
 };
-
-app.use(cors(corsOptions)); // CORS middleware ko use karo
 
 
 app.use(cors(corsOptions));
 
+
 // Middleware setup
-app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("API is working!");
 });
